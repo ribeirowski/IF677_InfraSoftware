fd0c5c750601f087f732d02b4a9877a9fb50018c
Matheus Augusto <62677004+MatheusMnt@users.noreply.github.com>
2023-03-25 06:59:41 -0300

    ajustes da Questão 3
    Adição de comentários e detecção de erro de abertura de arquivos

--- Projeto2_Threads/Quest_3/Quest_3.c
+++ Projeto2_Threads/Quest_3/Quest_3.c
@@ -6,18 +6,23 @@ typedef struct{
   int R, G, B;
 } pixel;
 
-void *ToGray(void *arg) {
-  pixel *px = (pixel*) arg;
-  int C = (px->R * 0.30) + (px->G * 0.59) + (px->B * 0.11);
+void *ToGray(void *arg) { //ToGray função de rotina da Thread
+  pixel *px = (pixel*) arg;//conversão de cada célula da matriz de pixels do tipo void para um ponteiro pixel
+  int C = (px->R * 0.30) + (px->G * 0.59) + (px->B * 0.11);  //formula dde conversao dada na questão
+
+  //aqui atualiza o valor de cada elemento RGB da celula da matriz
   px->R = C;
   px->G = C;
   px->B = C;
 
+  //aqui a thread é morta pois ja cumpriu sua função
   pthread_exit(NULL);
 }
 
 int main(void) {
-  
+
+
+  //criação do ponteiro para o arquivo de entrada 
   FILE *in;
   in = fopen("entrada.txt", "r+");
   if (in == NULL) {
@@ -25,15 +30,17 @@ int main(void) {
     exit(-1);
   }
 
+  //ppm recebe o tipo de arquivo de ppm, na questão dada o tipo é P3
   char ppm[3];
   fscanf(in, " %s", ppm);
 
+  //altura e largura da matriz
   int height, width;
   fscanf(in, " %d %d", &height, &width);
 
-  pixel matriz[height][width];
+  pixel matriz[height][width]; //alocação estática da matriz pois ela tem tamanho fixo 
 
-  int num_thread = height * width;
+  int num_thread = height * width; // o numero de threadas é a quantidade de celulas da matriz
   pthread_t threads[num_thread];
 
   int max_val;
@@ -42,14 +49,18 @@ int main(void) {
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
       fscanf(in, "%d %d %d ", &matriz[i][j].R, &matriz[i][j].G, &matriz[i][j].B);
-      pthread_create(&(threads[i + j]), NULL, ToGray, (void*) &(matriz[i][j]));
+      pthread_create(&(threads[i + j]), NULL, ToGray, (void*) &(matriz[i][j]));//para cada elemento da matriz é criada uma thread de conversao 
     }
   }
 
   FILE *out = fopen("saida.txt", "w+");
+  if(out == NULL){
+    printf("erro na criacao do arquivo de saida");
+    exit(-1);
+  }
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
-      fprintf(out, "%d %d %d\n", matriz[i][j].R, matriz[i][j].G, matriz[i][j].B);
+      fprintf(out, "%d %d %d\n", matriz[i][j].R, matriz[i][j].G, matriz[i][j].B); //pega o valor atualizado pelas threads e envia escreve no arquivo
     }
   }
 
