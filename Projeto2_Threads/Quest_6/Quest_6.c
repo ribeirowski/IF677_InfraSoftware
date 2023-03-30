8a348d99422aa61753ee15a0e645cf1bbbcd41d7
Enio Henrique <enioribeiro9@hotmail.com>
2023-03-29 23:51:46 -0300

    update: questão 6 ok

--- Projeto2_Threads/Quest_6/Quest_6.c
+++ Projeto2_Threads/Quest_6/Quest_6.c
@@ -1,7 +1,7 @@
 #include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
 #define T_MAX_BUF 10
@@ -9,8 +9,8 @@
 int ID = 0;
 pthread_t Thread[N];
 bool Exe[N] = {false};
-pthread_mutex_t mutex1, mutex2, mutex3 = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t empty, full, ready = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex1, mutex2, mutex3, mutex4 = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t empty, full, ready, pronto = PTHREAD_COND_INITIALIZER;
 
 typedef struct {
     int A;
@@ -19,7 +19,7 @@ typedef struct {
 } Parameters;
 
 typedef struct lista {
-    void *(*function)(void*);
+    void *(*function)(void *);
     int ID;
     Parameters P;
     struct lista *next;
@@ -30,19 +30,18 @@ typedef struct {
     List *Head, *Tail;
 } Buffer;
 
-typedef struct list {
-    int ID, R;
-    struct list *next;
-} Res;
-
 typedef struct {
-    Res *Head, *Tail;
-} Buff;
+    int R;
+    bool Exe;
+    bool ready;
+} Result;
 
 Buffer B;
-Buff Result;
+Result *R;
 
-void Insert_Buffer(void *(*function)(void*), Parameters P){ // Insere a função, o parametro e o id no Buffer
+void Insert_Buffer(
+    void *(*function)(void *),
+    Parameters P) { // Insere a função, o parametro e o id no Buffer
     List *New = (List *)malloc(sizeof(List));
     New->function = function;
     New->P = P;
@@ -50,9 +49,12 @@ void Insert_Buffer(void *(*function)(void*), Parameters P){ // Insere a função
     New->next = B.Tail->next;
     B.Tail->next = New;
     B.Tail = New;
+    // printf("ENTROUUUUUUUUU %d\n", ID);
     (B.Tam)++;
     ID++;
-    if(B.Tam == 1){
+    R = (Result *)realloc(R, sizeof(Result) * ID);
+    R[ID - 1].ready = 0;
+    if (B.Tam == 1) {
         pthread_cond_signal(&empty);
     }
 }
@@ -67,42 +69,15 @@ List Delete_Buffer() { // Deleta a função do buffer
         B.Tail = B.Head;
     }
     (B.Tam)--;
-    if(B.Tam == (B.TamMax)-1){
+    if (B.Tam == (B.TamMax) - 1) {
         pthread_cond_signal(&full);
     }
     return Ret;
 }
 
-void Insert_Result(int R, int Id){ //Insere o reusltado na fila de resultados
-    Res *new = (Res *)malloc(sizeof(Res));
-    new->ID = Id;
-    new->R = R;
-    new->next = Result.Tail->next;
-    Result.Tail->next = new;
-    Result.Tail = new;
-    pthread_cond_signal(&ready);
-}
-<<<<<<< HEAD
-
-=======
-                                      
->>>>>>> c5e97126f2387c0947a44b24273da7add2ef0857
-Res Delete_Result(Res *Cur){ //Deleta um resultado da fila de resultados
-    Res Ret, *Del;
-    Del = Cur->next;
-    Cur->next = Del->next;
-    Ret = *Del;
-    free(Del);
-    if(Result.Head->next == NULL){
-        Result.Tail = Result.Head;
-    }
-    return Ret;
-}
-
-void *mmc(void *P){
-<<<<<<< HEAD
+void *mmc(void *P) {
     int resto, a, b;
-    List num = *(List *)P; 
+    List num = *(List *)P;
     pthread_mutex_unlock(&mutex3);
     a = num.P.A;
     b = num.P.B;
@@ -111,96 +86,74 @@ void *mmc(void *P){
         a = b;
         b = resto;
     } while (resto != 0);
-    pthread_mutex_lock(&mutex3);
-    Insert_Result((num.P.A * num.P.B) / a, num.ID);
+    R[num.ID].R = (num.P.A * num.P.B) / a;
+    R[num.ID].Exe = 0;
+    R[num.ID].ready = 1;
+    pthread_cond_signal(&pronto);
     Exe[num.P.Thread_id] = false;
-    pthread_mutex_unlock(&mutex3);
     pthread_exit(NULL);
-=======
-  int resto, a, b;
-  List num = *(List *)P; 
-  pthread_mutex_unlock(&mutex3);
-      a = num.P.A;
-      b = num.P.B;
-      do {
-          resto = a % b;
-          a = b;
-          b = resto;
-      } while (resto != 0);
-      pthread_mutex_lock(&mutex3);
-      Insert_Result((num.P.A * num.P.B) / a, num.ID);
-      Exe[num.P.Thread_id] = false;
-      pthread_mutex_unlock(&mutex3);
-      pthread_exit(NULL);
->>>>>>> c5e97126f2387c0947a44b24273da7add2ef0857
 }
 
-void *mdc(void *P){
+void *mdc(void *P) {
     int resto;
-    List num = *(List *)P; 
+    List num = *(List *)P;
     pthread_mutex_unlock(&mutex3);
     do {
         resto = num.P.A % num.P.B;
         num.P.A = num.P.B;
         num.P.B = resto;
     } while (resto != 0);
-    pthread_mutex_lock(&mutex3);
-    Insert_Result(num.P.A, num.ID);
+    R[num.ID].R = num.P.A;
+    R[num.ID].Exe = 0;
+    R[num.ID].ready = 1;
+    pthread_cond_signal(&pronto);
     Exe[num.P.Thread_id] = false;
-    pthread_mutex_unlock(&mutex3);
     pthread_exit(NULL);
-}
-
-int agendarExecucao(void *(*function)(void*), Parameters P) {
-    if(B.Tam == B.TamMax){
-        pthread_mutex_lock(&mutex1);
-        pthread_cond_wait(&full, &mutex1);
-        pthread_mutex_unlock(&mutex1);
     }
+
+    int agendarExecucao(void *(*function)(void *), Parameters P) {
+    pthread_mutex_lock(&mutex1);
+    if (B.Tam == B.TamMax) {
+        pthread_cond_wait(&full, &mutex1);
+    }
+    pthread_mutex_unlock(&mutex1);
     Insert_Buffer(function, P);
     return B.Tail->ID; // retorna o id sequencial
 }
 
-void *Handler(){
+void *Handler() {
     List Task;
     bool Escolha;
-    while(true){
+    while (true) {
+        pthread_mutex_lock(&mutex1);
         Escolha = false;
-        if(B.Tam == 0){
-            pthread_mutex_lock(&mutex1);
+        if (B.Tam == 0) {
             pthread_cond_wait(&empty, &mutex1);
-            pthread_mutex_unlock(&mutex1);
         }
         pthread_mutex_lock(&mutex3);
         Task = Delete_Buffer();
-        while(!Escolha){
-            for(int a = 0; a < N && !Escolha; a++){
-                if(Exe[a] == false){
+        while (!Escolha) {
+            for (int a = 0; a < N && !Escolha; a++) {
+                if (Exe[a] == false) {
                     Exe[a] = true;
                     Task.P.Thread_id = a;
-                    pthread_create(&Thread[a], NULL, Task.function, (void*)&Task);
+                    // printf("entrei na thread de %d\n", a);
+                    pthread_create(&Thread[a], NULL, Task.function, (void *)&Task);
                     Escolha = true;
                 }
             }
         }
+        pthread_mutex_unlock(&mutex1);
     }
 }
 
-int pegarResultadoExecucao(int id){
-    Res *Aux = Result.Head;
-    Res Ret;
-    while(true){
-        if(Aux->next == NULL){
-            pthread_mutex_lock(&mutex2);
-            pthread_cond_wait(&ready, &mutex2);
-            pthread_mutex_unlock(&mutex2);
-        }
-        if(Aux->next->ID == id){
-            Ret = Delete_Result(Aux);
-            return Ret.R;
-        }
-        Aux = Aux->next;
+int pegarResultadoExecucao(int id) {
+    pthread_mutex_lock(&mutex3);
+    while (!R[id].ready) {
+        pthread_cond_wait(&pronto, &mutex3);
     }
+    pthread_mutex_unlock(&mutex3);
+    return R[id].R;
 }
 
 int main() {
@@ -210,59 +163,31 @@ int main() {
     B.Tail = B.Head;
     B.Head->next = NULL;
     B.Tam = 0;
-    Result.Head = (Res *)malloc(sizeof(Res));
-    Result.Tail = Result.Head;
-    Result.Head->next = NULL;
+    R = (Result *)calloc(1, sizeof(Result));
     pthread_t Despache;
     pthread_create(&Despache, NULL, Handler, NULL);
-    //Agendar as funções
-    FILE * input = fopen("entrada.txt", "r+");
-    if(input == NULL){
-<<<<<<< HEAD
+    // Agendar as funções
+    FILE *input = fopen("entrada.txt", "r+");
+    if (input == NULL) {
         printf("falha ao abrir o arquivo de entrada");
-            exit(-1);
-=======
-      printf("falha ao abrir o arquivo de entrada");
-      exit(-1);
->>>>>>> c5e97126f2387c0947a44b24273da7add2ef0857
+        exit(-1);
     }
-
     char op[4];
-
     do {
-<<<<<<< HEAD
-    int id;
-    fscanf(input," %s", op);
-    if(strcmp(op, "REQ") == 0){
-        fscanf(input, " %d", &id);
-        printf("Resultado %d = %d\n", id, pegarResultadoExecucao(id));
-    }else if(strcmp(op, "MMC") == 0){
-        fscanf(input, " %d %d", &P.A, &P.B);
-        printf("%s %d %d ID: %d\n",op, P.A, P.B, agendarExecucao(mmc, P));
-    }else if(strcmp(op, "MDC") == 0){
-        fscanf(input, " %d %d", &P.A, &P.B);
-        printf("%s %d %d ID: %d\n",op, P.A, P.B, agendarExecucao(mdc, P));
-    }
-=======
-      int id;
-      fscanf(input," %s", op);
-      if(strcmp(op, "REQ") == 0){
-        fscanf(input, " %d", &id);
-        printf("Resultado %d = %d\n", id, pegarResultadoExecucao(id));
-      }else if(strcmp(op, "MMC") == 0){
-        fscanf(input, " %d %d", &P.A, &P.B);
-        printf("%s %d %d ID: %d\n",op, P.A, P.B, agendarExecucao(mmc, P));
-      }else if(strcmp(op, "MDC") == 0){
-        fscanf(input, " %d %d", &P.A, &P.B);
-        printf("%s %d %d ID: %d\n",op, P.A, P.B, agendarExecucao(mdc, P));
-      }
->>>>>>> c5e97126f2387c0947a44b24273da7add2ef0857
-    }while(strcmp(op, "END") != 0);
+        int id;
+        fscanf(input, " %s", op);
+        if (strcmp(op, "REQ") == 0) {
+            fscanf(input, " %d", &id);
+            printf("Resultado %d = %d\n", id, pegarResultadoExecucao(id));
+        } else if (strcmp(op, "MMC") == 0) {
+            fscanf(input, " %d %d", &P.A, &P.B);
+            printf("%s %d %d ID: %d\n", op, P.A, P.B, agendarExecucao(mmc, P));
+        } else if (strcmp(op, "MDC") == 0) {
+            fscanf(input, " %d %d", &P.A, &P.B);
+            printf("%s %d %d ID: %d\n", op, P.A, P.B, agendarExecucao(mdc, P));
+        }
+    } while (strcmp(op, "END") != 0);
     fclose(input);
-    pthread_exit(NULL);
+    //printf("CABO\n");
     return 0;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> c5e97126f2387c0947a44b24273da7add2ef0857
